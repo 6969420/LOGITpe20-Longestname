ccbcdba1255c3e7e895a01738ce3f765af01ab3b
Opilane TTHK <opilane@tthk.ee>
2020-09-24 12:37:31 +0300

    Create Threeriis

---
+++ WhileLoop/Threeries/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Threeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            int userInput = Int32.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            while (counter < 3)
+            {
+                if (randomNumber == userInput)
+                {
+                    Console.WriteLine("Congratlations, you have won!");
+                    break;
+                } else
+                {
+                    Console.WriteLine("Try again!");
+                     counter = counter + 1;
+
+                }
+            }
+        }
+        
+    }
+}
